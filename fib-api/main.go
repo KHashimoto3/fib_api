0ec1132a6ad809f77ea413bebe5ba5a70c386949
KHashimoto3 <hashi.koki385@gmail.com>
2023-11-09 16:12:13 +0900

    fibのパスに対する関数を仮で用意した。

--- fib-api/main.go
+++ fib-api/main.go
@@ -6,13 +6,26 @@ import (
 
 func hello(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"message":"Hello World",
+		"message":"hello! world!",
 	})
 }
+
+func getFib(c *gin.Context) {
+	answer := fib(400)
+	c.JSON(200, gin.H{
+		"result": answer,
+	})
+}
+
+func fib(n int32) int32 {
+	return 500
+}
+
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/", hello)
+	router.GET("/fib", getFib)
 	router.Run(":8080")
 }
